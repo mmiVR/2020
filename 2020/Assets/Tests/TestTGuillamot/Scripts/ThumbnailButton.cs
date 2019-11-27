471ad7f94b9c55273c4db9c54a3b24aae81def3d
Farewell_ <norththelittlefox@gmail.com>
2019-11-27 02:06:01 +0100

    Canvas Thumbnails
    Un affichage basique de thumbnails sur un canvas, les fonctions pour le player 360 sont présentes, il ne reste plus qu'a le coder... la suite au prochain episode

---
+++ 2020/Assets/Tests/TestTGuillamot/Scripts/ThumbnailButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThumbnailButton : Button //Initialisation du bouton
+{
+    public string videoURL; //Url de la video a lire
+
+
+    protected override void Awake()
+    {
+        //Code pour init le projo (ptdr aled)
+    }
+
+    protected override void Start()
+    {
+        onClick.AddListener(PlayVideo); //Execute la fonction PlayVideo lorsque le bouton est cliqué
+    }
+
+    private void PlayVideo()
+    {
+        //Oui super, ça va etre relou
+        print("PLAY : " + videoURL);
+        //Mais l'argument passe alors c'est good
+    }
+}

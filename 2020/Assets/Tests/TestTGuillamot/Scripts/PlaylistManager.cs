e500ee2f8ce8c8b567ab2b29cbf61937573492ce
Farewell_ <norththelittlefox@gmail.com>
2019-11-28 04:53:09 +0100

    Lecteur Video 360 (push sans video)
    Le lecteur est fonctionnel, en revanche, le canvas est toujours visible pendant la lecture : ce sera réglé lors du prochain push de ma part (il suffit de set le state de l'objet canvas sur false)
    
    L'effet 360 est atteint grace a un set de la skybox globale. Cela posera plusieurs problemes pour le jeu (En revanche, cela peut etre modifié en utilisant comme render mode "nearCameraPlane", un fix tres simple a instaurer)
    
    Le code est entièrement commenté et devrait etre facilement modifiable.

--- 2020/Assets/Tests/TestTGuillamot/Scripts/PlaylistManager.cs
+++ 2020/Assets/Tests/TestTGuillamot/Scripts/PlaylistManager.cs
@@ -2,32 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
-public class PlaylistManager : MonoBehaviour 
+public class PlaylistManager : MonoBehaviour //Initialisation du manager
 {
-    public GameObject thumbnailPrefab; 
+    public GameObject thumbnailPrefab;       //On passe le prefab des boutons
 
-    public List<Sprite> thumbnails;
-    public List<string> videoUrls;
+    public RenderTexture renderTexture;      //On passe la texture de rendu
+    public Material renderMaterial;          //On passe le materiel de rendu pour la skybox
 
-    private Dictionary<Sprite, string> playlist;
+    public bool playing = false;             ///On s'en servira plus tard (Permettra de faire disparaitre le canvas lors de la lecture)
+
+    public List<Sprite> thumbnails;          //Liste contenant les images des thumbnails
+    public List<VideoClip> videos;           //Liste contenant les Videos
+
+    private Dictionary<Sprite, VideoClip> playlist;    //Playlist contenant les thumbnails et les videos
+
+    [HideInInspector] public VideoPlayer videoPlayer;  //Objet lecteur de video
+    [HideInInspector] public AudioSource audioSource;  //Objet source audio
+
+    private GameObject projector;            //Objet dans le quel le lecteur audio et videos seront crées, permet de garder un hierarchie plus propre
 
     private void Awake()
     {
-        playlist = new Dictionary<Sprite, string>();
-        for (int i = 0; i < thumbnails.Count; i++)
+        playlist = new Dictionary<Sprite, VideoClip>();
+        if (thumbnails.Count == videos.Count)            //Test pour s'assurer qu'on declare autant de video que de thumbnails
+        {                                                //Si c'est le cas, on crée le lecteur video / son et on crée la liste des boutons a afficher
+            for (int i = 0; i < thumbnails.Count; i++)   //Pour chaque element de la liste, on...
+            {
+                playlist.Add(thumbnails[i], videos[i]);  //...Attribue a playlist[i] la thumbnail et la video que l'on veux afficher, il y a autant de cases dans playlist que de boutons
+            }
+            Debug.Log("<b>Disctionnaire de vidéos crée : <color=magenta>" + thumbnails.Count + " éléments chargés</color></b>");
+
+            //Code pour init le projo (ptdr aled)
+            projector = new GameObject("Projecteur Video 360");                       //Création de l'objet projector, comme expliqué plus haut, il permet de garder un hierarchie plus propre
+            videoPlayer = projector.AddComponent<VideoPlayer>();                      //On ajoute le lecteur video a cet objet
+            audioSource = projector.AddComponent<AudioSource>();                      //On ajoute le lecteur audio a cet objet
+
+            
+            videoPlayer.playOnAwake = false; audioSource.playOnAwake = false;         //On empeche le lancement automatique des medias
+            videoPlayer.source = VideoSource.VideoClip;                               //On veux lire la video depuis un fichier et pas une url
+            videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;           //On set la source audio en mode output
+            videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture; //On set le mode de rendu en mode rendu texture (Pour pouvoir streamer la video sur une texture)
+            videoPlayer.EnableAudioTrack(0, true);                                    //On assigne le son de la video a notre source audio
+            videoPlayer.SetTargetAudioSource(0, audioSource);
+
+            Debug.Log("<b>Le player a été initialisé. (got : " + videoPlayer + ").</b>");
+        }
+        else
         {
-            playlist.Add(thumbnails[i], videoUrls[i]);
+            Debug.Log("<b><color=red>Erreur lors de la création du dictionnaire : le nombre de videos et de thumbnails ne correspondent pas</color></b>");
         }
     }
 
-    private void Start()
+    public void Start()
     {
-        foreach (KeyValuePair<Sprite, string> playlistItem in playlist)
+        foreach (KeyValuePair<Sprite, VideoClip> playlistItem in playlist)                  //Pour chacuns de nos boutons...
         {
-            GameObject newThumbnail = Instantiate(thumbnailPrefab, transform);
-            newThumbnail.GetComponent<Image>().sprite = playlistItem.Key;
-            newThumbnail.GetComponent<ThumbnailButton>().videoURL = playlistItem.Value;
+            GameObject newThumbnail = Instantiate(thumbnailPrefab, transform);              //On cree un nouveau objet bouton
+            newThumbnail.GetComponent<Image>().sprite = playlistItem.Key;                   //On set sa thumbnail
+            newThumbnail.GetComponent<ThumbnailButton>().videos = playlistItem.Value;       //On set la video qu'il permettra de lire
+            newThumbnail.GetComponent<ThumbnailButton>().renderTexture = renderTexture;     //On set la texture sur la quelle il va rendre la video
+            newThumbnail.GetComponent<ThumbnailButton>().renderMaterial = renderMaterial;   //On set le materiel qui servira a update la skybox
+            Debug.Log("<b>Element " + playlistItem.Value + " crée.</b>");                   //Puis on logue sa création dans la console :)
         }
     }
 }
--- 2020/Assets/Tests/TestTGuillamot/Scripts/ThumbnailButton.cs
+++ 2020/Assets/Tests/TestTGuillamot/Scripts/ThumbnailButton.cs
@@ -2,26 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
-public class ThumbnailButton : Button //Initialisation du bouton
+public class ThumbnailButton : Button     //Initialisation du bouton
 {
-    public string videoURL; //Url de la video a lire
+    [HideInInspector] public VideoClip videos;              //Video a lire
+
+    [HideInInspector] public RenderTexture renderTexture;   //On recupere la texture de rendu
+    [HideInInspector] public Material renderMaterial;       //On recupere le materiel de rendu pour la skybox
+
+    private VideoPlayer videoPlayer;      //Objet lecteur de video
+    private AudioSource audioSource;      //Objet source audio
 
 
     protected override void Awake()
     {
-        //Code pour init le projo (ptdr aled)
+        videoPlayer = gameObject.GetComponentInParent<PlaylistManager>().videoPlayer;   //On recupere le lecteur video
+        audioSource = gameObject.GetComponentInParent<PlaylistManager>().audioSource;   //On recupere le lecteur audio
     }
 
     protected override void Start()
     {
-        onClick.AddListener(PlayVideo); //Execute la fonction PlayVideo lorsque le bouton est cliqué
+        onClick.AddListener(PlayVideo);   //Execute la fonction PlayVideo lorsque le bouton est cliqué
     }
 
     private void PlayVideo()
     {
-        //Oui super, ça va etre relou
-        print("PLAY : " + videoURL);
-        //Mais l'argument passe alors c'est good
+        RenderSettings.skybox = renderMaterial;      //Set le rendu de la skybox avec le materiel sur le quel est streamé la video, c'est ceci qui permet d'afficher la video a 360 degrés
+
+        videoPlayer.targetTexture = renderTexture;   //Set la texture sur la quelle la video va streamer ses frames
+        videoPlayer.clip = videos;                   //Set la video a charger
+        videoPlayer.Prepare();                       //Pre charge le son pour eviter des problemes de buffering
+
+
+        videoPlayer.Play();                          //Lance la video
+        audioSource.Play();                          //lance l' audio
+
+        gameObject.GetComponentInParent<PlaylistManager>().playing = true;   //Set la variable "playing" du playlistManager sur true
+        Debug.Log("<b>Lancement de la lecture de la video " + videos.name + "</b>");
     }
 }
